ae0529b4e624c90928d160c0fbabbde701cfcd55
Leo Borrero <leonardo.borrero2@studytafensw.edu.au>
2026-03-17 15:33:46 +1100

    checkpoint particle system

--- Unity Files/Bun Run/Assets/Scripts/Checkpoint.cs
+++ Unity Files/Bun Run/Assets/Scripts/Checkpoint.cs
@@ -4,6 +4,7 @@ public class Checkpoint : MonoBehaviour
 {
     public RespawnManager respawnManager;
     public GameObject self;
+    public GameObject particleParent;
     private void Awake()
     {
         self = gameObject;
@@ -12,6 +13,17 @@ public class Checkpoint : MonoBehaviour
     {
         respawnManager.storedLocation = transform.position;
         respawnManager.checkpointCount += 1;
+        
+        ParticleSystem[] particles = particleParent.GetComponentsInChildren<ParticleSystem>();
+        // detach particles 
+        particleParent.transform.parent = null;
+        
+        foreach (ParticleSystem ps in particles)
+        {
+            ps.Play();
+        }
+        
+        Destroy(particleParent, 5f);
         Destroy(self);
     }
 }
